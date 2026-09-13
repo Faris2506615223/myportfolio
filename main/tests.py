6840276bfb58ce9b4e6d284be64c40647c9bd32e
Faris <faris.salman@ui.ac.id>
2026-09-13 10:47:18 +0700

    test(main): add comprehensive unit tests for project list, detail view, and 404 handling

--- main/tests.py
+++ main/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from main.models import Experience
+from main.models import Experience, Project
 
 
 class MainTest(TestCase):
@@ -12,6 +12,13 @@ class MainTest(TestCase):
             description="Membantu mahasiswa memahami pengembangan web.",
             category="part-time",
         )
+        self.project = Project.objects.create(
+            title="SimKopDes 2.0",
+            category="Web App • Dashboard",
+            description="Sistem informasi manajemen koperasi desa berbasis web.",
+            thumbnail="/static/img/projects/Simkopdes2.0.webp",
+            project_url="https://github.com/Faris2506615223",
+        )
 
     def test_main_url_is_accessible(self):
         response = self.client.get(reverse("main:show_main"))
@@ -20,6 +27,7 @@ class MainTest(TestCase):
         self.assertTemplateUsed(response, "index.html")
         self.assertNotContains(response, self.experience.title)
         self.assertContains(response, f'href="{reverse("main:show_experience")}"')
+        self.assertContains(response, f'href="{reverse("main:show_projects")}"')
 
     def test_nonexistent_page_returns_404(self):
         response = self.client.get("/halaman-yang-tidak-ada/")
@@ -41,6 +49,7 @@ class MainTest(TestCase):
         self.assertContains(response, "Part-Time")
         self.assertContains(response, "Sedang berlangsung")
         self.assertContains(response, f'href="{reverse("main:show_main")}"')
+        self.assertContains(response, f'href="{reverse("main:show_projects")}"')
 
     def test_empty_experience_page(self):
         Experience.objects.all().delete()
@@ -56,3 +65,53 @@ class MainTest(TestCase):
         self.assertFalse(self.experience.is_ongoing)
         self.assertContains(response, "Selesai")
         self.assertNotContains(response, "Sedang berlangsung")
+
+    def test_project_model(self):
+        self.assertEqual(str(self.project), "SimKopDes 2.0")
+        self.assertEqual(self.project.category, "Web App • Dashboard")
+        self.assertEqual(self.project.thumbnail, "/static/img/projects/Simkopdes2.0.webp")
+
+    def test_projects_url_is_accessible_and_uses_correct_template(self):
+        response = self.client.get(reverse("main:show_projects"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "projects.html")
+        self.assertContains(response, f'href="{reverse("main:show_main")}"')
+        self.assertContains(response, f'href="{reverse("main:show_experience")}"')
+        self.assertContains(response, f'href="{reverse("main:show_projects")}"')
+
+    def test_projects_page_displays_data(self):
+        response = self.client.get(reverse("main:show_projects"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.project.title)
+        self.assertContains(response, self.project.category)
+        self.assertContains(response, self.project.description)
+        self.assertContains(response, self.project.thumbnail)
+        self.assertContains(response, self.project.project_url)
+        self.assertContains(response, f'href="{reverse("main:show_project_detail", args=[self.project.id])}"')
+
+    def test_empty_projects_page(self):
+        Project.objects.all().delete()
+        response = self.client.get(reverse("main:show_projects"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Belum ada proyek yang ditambahkan.")
+
+    def test_project_detail_url_is_accessible_and_uses_correct_template(self):
+        response = self.client.get(reverse("main:show_project_detail", args=[self.project.id]))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "project_detail.html")
+        self.assertContains(response, self.project.title)
+        self.assertContains(response, self.project.category)
+        self.assertContains(response, self.project.description)
+        self.assertContains(response, self.project.project_url)
+        self.assertContains(response, f'href="{reverse("main:show_projects")}"')
+
+    def test_project_detail_nonexistent_returns_404(self):
+        import uuid
+        random_uuid = uuid.uuid4()
+        response = self.client.get(reverse("main:show_project_detail", args=[random_uuid]))
+
+        self.assertEqual(response.status_code, 404)
