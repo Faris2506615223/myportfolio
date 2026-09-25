fa0d3d8b892ee77a08103c7e9234195f0d55aa29
Faris <faris.salman@ui.ac.id>
2026-09-25 11:14:30 +0700

    feat(projects): restrict add/delete to superuser, add toggle star feature and natural foreign keys

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 import uuid
+from django.contrib.auth.models import User
 from django.db import models
 
 class Experience(models.Model):
@@ -39,6 +40,9 @@ class Project(models.Model):
     thumbnail = models.CharField(max_length=255, blank=True, null=True)
     project_url = models.URLField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    starred_by = models.ManyToManyField(
+        User, related_name="starred_projects", blank=True
+    )
 
     def __str__(self):
         return self.title
